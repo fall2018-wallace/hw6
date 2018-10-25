ef9a662f05522e0477b3dc2b5db18d6a8bff03af
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 15:00:28 -0400

    Saving maps.R

--- maps.R
+++ maps.R
@@ -1,8 +1,8 @@
 
 library(ggplot2)
-hist1 <- ggplot(data = merged_data, aes(merged_data$population))+
-  geom_histogram(color ='black', fill= 'white' , bins = 50)
-hist1
+#hist1 <- ggplot(data = merged_data, aes(merged_data$population))+
+#  geom_histogram(color ='black', fill= 'white' , bins = 50)
+#hist1
 hist2 <- ggplot(data = merged_data,aes(merged_data$Murder))+
   geom_histogram(color ='black', fill= 'white' ,bins = 50)
 hist2

878d371475a31b99d0f2ab6ace1d97acd4482fcd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:43:24 -0400

    Saving maps.R

--- maps.R
+++ maps.R
@@ -4,7 +4,7 @@ hist1 <- ggplot(data = merged_data, aes(merged_data$population))+
   geom_histogram(color ='black', fill= 'white' , bins = 50)
 hist1
 hist2 <- ggplot(data = merged_data,aes(merged_data$Murder))+
-  geom_histogram(color ='black', fill= 'white' bins = 50)
+  geom_histogram(color ='black', fill= 'white' ,bins = 50)
 hist2
 ##5)	Create a boxplot for the population, and a different boxplot for the murder rate.
 
